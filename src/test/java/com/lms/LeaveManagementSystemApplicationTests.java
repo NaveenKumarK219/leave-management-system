137ee563b7df98b420de03d787f771a1b92aac77
Navin Kumar K <gangadari.naveen@gmail.com>
2019-06-11 15:55:45 +0530

    Test cases removed

--- src/test/java/com/lms/LeaveManagementSystemApplicationTests.java
+++ src/test/java/com/lms/LeaveManagementSystemApplicationTests.java
@@ -1,15 +1,12 @@
 package com.lms;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+/*@RunWith(SpringRunner.class)
+@SpringBootTest*/
 public class LeaveManagementSystemApplicationTests {
 
-	@Test
+	//@Test
 	public void contextLoads() {
 	}
 
